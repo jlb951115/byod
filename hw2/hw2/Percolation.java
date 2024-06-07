e7b358c40e1428849c79c4dd82b8508a3b4ed213
jlb951115 <3065649450@qq.com>
2024-06-07 09:01:45 +0800

    completed the hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,78 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int n;
+    private int[][] array;
+    private WeightedQuickUnionUF rowSet;
+    private WeightedQuickUnionUF set;
+    private int count;
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("Illegal Argument");
+        }
+        n = N;
+        array = new int[N][N];
+        rowSet = new WeightedQuickUnionUF(N);
+        set = new WeightedQuickUnionUF(N * N + 1);
+        count = 0;
+        for (int i = 0; i < N; i++) {
+            set.union(i, N * N);
+        }
+    }
 
+    public boolean isOpen(int row, int col) {
+        if (row >= n || col >= n) {
+            throw new IndexOutOfBoundsException("Out of Bounds");
+        }
+        return array[row][col] == 1;
+    }
+
+    private int getIndex(int row, int col) {
+        return row * n + col;
+    }
+
+    public void open(int row, int col) {
+        if (row >= n || col >= n) {
+            throw new IndexOutOfBoundsException("Out of Bounds");
+        }
+        if (isOpen(row, col)) {
+            return;
+        }
+        array[row][col] = 1;
+        count += 1;
+        int index = getIndex(row, col);
+        for (int i = row - 1; i <= row + 1; i++) {
+            if (i >= n || i < 0) {
+                continue;
+            }
+            if (isOpen(i, col)) {
+                rowSet.union(i, row);
+                set.union(getIndex(i, col), index);
+            }
+        }
+
+        for (int i = col - 1; i <= col + 1; i++) {
+            if (i >= n || i < 0) {
+                continue;
+            }
+            if (isOpen(row, i)) {
+                set.union(getIndex(row, i), index);
+            }
+        }
+    }
+
+    public boolean isFull(int row, int col) {
+        if (row >= n || col >= n) {
+            throw new IndexOutOfBoundsException("Out of Bounds");
+        }
+        return set.connected(getIndex(row, col), n * n) && isOpen(row, col);
+    }
+
+    public int numberOfOpenSites() {
+        return count;
+    }
+
+    public boolean percolates() {
+        return rowSet.connected(0, n - 1) && count > 0;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,40 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+public class PercolationStats {
+    private int T;
+    private double[] experiment;
+    private static final double CONSTANT = 1.96;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        this.experiment = new double[T];
+        this.T = T;
+        for (int i = 0; i < T; i++) {
+            Percolation p = pf.make(N);
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                if (!p.isOpen(row, col)) {
+                    p.open(row, col);
+                }
+            }
+            experiment[i] = (double) (p.numberOfOpenSites()) / (double) (N * N);
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(experiment);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(experiment);
+    }
+
+    public double confidenceLow() {
+        return this.mean() - CONSTANT * this.stddev() / Math.sqrt(T);
+    }
+    public double confidenceHigh() {
+        return this.mean() + CONSTANT * this.stddev() / Math.sqrt(T);
+    }
 }
